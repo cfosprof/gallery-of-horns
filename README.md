dead9dd507c7fca0451ae6651f00a589a1f277cd
Christopher Foster <cfosprof@gmail.com>
2023-03-14 13:33:07 -0700

    okay

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 An application that displays images and information of horned animals, allowing users to filter images by number of horns and chose their favorite image.
-
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      favorites: 0,
+      favorited: false,
+    };
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      favorites: prevState.favorites + 1,
+      favorited: true,
+    }));
+  }
+
   render(){
     const { title, imageUrl, description } = this.props;
+    const { favorites } = this.state;
 
     return (
       <article>
         <h2>{title}</h2>
-        <img src={imageUrl} alt={title} title={title} />
+        <img
+          src={imageUrl}
+          alt={title}
+          title={title}
+          onClick={this.handleClick}
+        />
         <p>{description}</p>
+        <p>
+          <span role="img" aria-label = "heart">❤️</span>
+          {favorites}
+        </p>
       </article>
     )
   }
